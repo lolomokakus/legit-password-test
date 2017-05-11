7ba9141396f2e14418275d135e29e7b4aa1d0467
zeroxfourc <ludvighg1999@gmail.com>
2017-05-11 09:01:27 +0200

    first testing

--- results.php
+++ results.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <head>
   <meta charset="utf-8">
-  <?php
-    echo "lol saved that shit";
-  ?>
+  <title>Resultat – Testa ditt lösenord!</title>
+  <link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
+  <p>hej ditt lösenord är <?php echo $_POST["password"] ?> lol</p>
 </body>
