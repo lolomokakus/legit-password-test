f37e625aeda7f54a28d267bb40ea1b457af6156b
zeroxfourc <ludvighg1999@gmail.com>
2017-05-23 08:51:12 +0200

    invent scoring

--- results.php
+++ results.php
@@ -13,19 +13,19 @@
         <?php
           $password = $_POST["password"];
           $lower = "abcdefghijklmnopqrstuvwxyz";
-          $contains = 0;
+          $contains_lower = 0;
           for($x = 0; $x < strlen($password); $x++) {
             for($y = 0; $y < strlen($lower); $y++) {
               if($password[$x] == $lower[$y]) {
-                echo ✓;
-                $contains = 1;
+                echo Ja;
+                $contains_lower = 1;
                 break 2;
-              };
-            };
-          };
-          if($contains == 0) {
-            echo ✗;
-          };
+              }
+            }
+          }
+          if($contains_lower == 0) {
+            echo Nej;
+          }
         ?>
       </td>
     </tr>
@@ -34,19 +34,19 @@
       <td>
         <?php
           $upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-          $contains = 0;
+          $contains_upper = 0;
           for($x = 0; $x < strlen($password); $x++) {
             for($y = 0; $y < strlen($upper); $y++) {
               if($password[$x] == $upper[$y]) {
-                echo ✓;
-                $contains = 1;
+                echo Ja;
+                $contains_upper = 1;
                 break 2;
-              };
-            };
-          };
-          if($contains == 0) {
-            echo ✗;
-          };
+              }
+            }
+          }
+          if($contains_upper == 0) {
+            echo Nej;
+          }
         ?>
       </td>
     </tr>
@@ -55,19 +55,19 @@
       <td>
         <?php
           $numbers = "0123456789";
-          $contains = 0;
+          $contains_number = 0;
           for($x = 0; $x < strlen($password); $x++) {
             for($y = 0; $y < strlen($numbers); $y++) {
               if($password[$x] == $numbers[$y]) {
-                echo ✓;
-                $contains = 1;
+                echo Ja;
+                $contains_number = 1;
                 break 2;
-              };
-            };
-          };
-          if($contains == 0) {
-            echo ✗;
-          };
+              }
+            }
+          }
+          if($contains_number == 0) {
+            echo Nej;
+          }
         ?>
       </td>
     </tr>
@@ -75,24 +75,72 @@
       <td>Innehåller symboler:</td>
       <td>
         <?php
-          $contains = 0;
+          $contains_symbol = 0;
           for($x = 0; $x < strlen($password); $x++) {
             for($y = 0; $y <= 255; $y++) {
-              if((65 <= $y and $y <= 90) or (97 <= $y and $y <= 122)) {
+              if((48 <= $y and $y <= 57) or (65 <= $y and $y <= 90) or (97 <= $y and $y <= 122)) {
                 break 1;
-              };
+              }
               if(ord($password[$x]) == $y) {
-                echo ✓;
-                $contains = 1;
+                echo Ja;
+                $contains_symbol = 1;
                 break 2;
-              };
-            };
-          };
-          if($contains == 0) {
-            echo ✗;
-          };
+              }
+            }
+          }
+          if($contains_symbol == 0) {
+            echo Nej;
+          }
+        ?>
+      </td>
+    </tr>
+    <tr>
+      <td>Längd:</td>
+      <td>
+        <?php
+          if(strlen(password) <= 8) {
+            echo Kort;
+            $length = 0;
+          } elseif(strlen(password) <= 20) {
+            echo Medellångt;
+            $length = 1;
+          } else {
+            echo Långt;
+            $length = 2;
+          }
         ?>
       </td>
     </tr>
   </table>
+  <h1>
+    Poäng: <?php
+      $score = 0;
+      if($contains_lower == 1) {
+        $score += 5;
+      } else {
+        $score -= 5;
+      }
+      if($contains_upper == 1) {
+        $score += 7;
+      } else {
+        $score -= 5;
+      }
+      if($contains_number == 1) {
+        $score += 7;
+      } else {
+        $score -= 5;
+      }
+      if($contains_symbol == 1) {
+        $score += 10;
+      } else {
+        $score -= 5;
+      }
+      if($length == 2) {
+        $score += 20;
+      } elseif($length == 0) {
+        $score -= 15;
+      }
+      echo $score;
+    ?>
+  </h1>
 </body>
